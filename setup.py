23cf7d859da8b1e722caada7d804aa008f89027f
Pavel Kozlov <mail@pkozlov.ru>
2015-06-08 09:22:30 +0300

    v 0.5

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mynewsdesk',
-    version='0.4',
+    version='0.5',
     packages=['mynewsdesk'],
     include_package_data=True,
     license='MIT License',
